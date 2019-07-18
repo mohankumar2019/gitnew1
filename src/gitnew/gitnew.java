f05656383e6696419253b686c585669de7a921a5
mohan <mohan.chanti4@gmail.com>
2019-07-18 19:35:10 +0530

    update file

--- src/gitnew/gitnew.java
+++ src/gitnew/gitnew.java
@@ -3,7 +3,7 @@ package gitnew;
 public class gitnew {
 
 	public static void main(String[] args) {
-		System.out.println("gitnew11111222222");
+		System.out.println("gitnew111112222223333333333333");
 
 	}
 
