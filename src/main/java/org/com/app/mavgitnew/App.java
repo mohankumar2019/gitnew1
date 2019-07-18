bf73364d5ab302725fb27cb50bb487a95616c230
mohan <mohan.chanti4@gmail.com>
2019-07-18 20:08:49 +0530

    new changes

---
+++ src/main/java/org/com/app/mavgitnew/App.java
@@ -0,0 +1,13 @@
+package org.com.app.mavgitnew;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "mavgitnew1" );
+    }
+}
